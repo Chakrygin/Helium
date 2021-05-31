819f0f3e598e9fed98c619f101aeb65825cd57ff
Igor Chakrygin <igor@chakrygin.ru>
2021-06-01 00:03:24 +0300

    Add mapping tests for tuple types

---
+++ tests/Helium.SqlServer.Tests/Mapping/TupleTypes/TupleType_Tuple_Tests.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Helium.SqlServer.Tests.Mapping.EntityTypes;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Helium.SqlServer.Tests.Mapping.TupleTypes
+{
+    [TestClass]
+    public sealed class TupleType_Tuple_Tests : EntityType_Base_Tests
+    {
+        [TestMethod]
+        public async Task Tuple_Exists_Test()
+        {
+            var result = await Database
+                .Query($@"
+                    select [Id], [Name]
+                    from [dbo].[{Table.Name}];")
+                .ExecuteAsync<Tuple<int, string>>();
+
+            var expected = Tuple.Create(Table.Row.Id, Table.Row.Name);
+
+            result.Print();
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public async Task Tuple_NotExists_Test()
+        {
+            var result = await Database
+                .Query($@"
+                    select [Id], [Name]
+                    from [dbo].[{Table.Name}]
+                    where [Id] < 0;")
+                .ExecuteAsync<Tuple<int, string>>();
+
+            result.Print();
+            result.Should().BeNull();
+        }
+
+        [TestMethod]
+        public async Task Tuple_OnlyFirstColumn_Test()
+        {
+            var result = await Database
+                .Query($@"
+                    select [Id]
+                    from [dbo].[{Table.Name}];")
+                .ExecuteAsync<Tuple<int, string>>();
+
+            var expected = Tuple.Create(Table.Row.Id, default(string));
+
+            result.Print();
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public async Task Tuple_ExtraColumn_Test()
+        {
+            var result = await Database
+                .Query($@"
+                    select [Id], [Name], [Date]
+                    from [dbo].[{Table.Name}];")
+                .ExecuteAsync<Tuple<int, string>>();
+
+            var expected = Tuple.Create(Table.Row.Id, Table.Row.Name);
+
+            result.Print();
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ tests/Helium.SqlServer.Tests/Mapping/TupleTypes/TupleType_ValueTuple_Tests.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Helium.SqlServer.Tests.Mapping.EntityTypes;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Helium.SqlServer.Tests.Mapping.TupleTypes
+{
+    [TestClass]
+    public sealed class TupleType_ValueTuple_Tests : EntityType_Base_Tests
+    {
+        [TestMethod]
+        public async Task ValueTuple_Exists_Test()
+        {
+            var result = await Database
+                .Query($@"
+                    select [Id], [Name]
+                    from [dbo].[{Table.Name}];")
+                .ExecuteAsync<(int, string)>();
+
+            var expected = (Table.Row.Id, Table.Row.Name);
+
+            result.Print();
+            result.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public async Task NullableValueTuple_Exists_Test()
+        {
+            var result = await Database
+                .Query($@"
+                    select [Id], [Name]
+                    from [dbo].[{Table.Name}];")
+                .ExecuteAsync<(int, string)?>();
+
+            var expected = (Table.Row.Id, Table.Row.Name);
+
+            result.Print();
+            result.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public async Task ValueTuple_NotExists_Test()
+        {
+            var result = await Database
+                .Query($@"
+                    select [Id], [Name]
+                    from [dbo].[{Table.Name}]
+                    where [Id] < 0;")
+                .ExecuteAsync<(int, string)>();
+
+            var expected = (default(int), default(string)!);
+
+            result.Print();
+            result.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public async Task NullableValueTuple_NotExists_Test()
+        {
+            var result = await Database
+                .Query($@"
+                    select [Id], [Name]
+                    from [dbo].[{Table.Name}]
+                    where [Id] < 0;")
+                .ExecuteAsync<(int, string)?>();
+
+            result.Print();
+            result.Should().BeNull();
+        }
+
+        [TestMethod]
+        public async Task ValueTuple_OnlyFirstColumn_Test()
+        {
+            var result = await Database
+                .Query($@"
+                    select [Id]
+                    from [dbo].[{Table.Name}];")
+                .ExecuteAsync<(int, string)>();
+
+            var expected = (Table.Row.Id, default(string)!);
+
+            result.Print();
+            result.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public async Task NullableValueTuple_OnlyFirstColumn_Test()
+        {
+            var result = await Database
+                .Query($@"
+                    select [Id]
+                    from [dbo].[{Table.Name}];")
+                .ExecuteAsync<(int, string)?>();
+
+            var expected = (Table.Row.Id, default(string));
+
+            result.Print();
+            result.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public async Task ValueTuple_ExtraColumn_Test()
+        {
+            var result = await Database
+                .Query($@"
+                    select [Id], [Name], [Date]
+                    from [dbo].[{Table.Name}];")
+                .ExecuteAsync<(int, string)>();
+
+            var expected = (Table.Row.Id, Table.Row.Name);
+
+            result.Print();
+            result.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public async Task NullableValueTuple_ExtraColumn_Test()
+        {
+            var result = await Database
+                .Query($@"
+                    select [Id], [Name], [Date]
+                    from [dbo].[{Table.Name}];")
+                .ExecuteAsync<(int, string)?>();
+
+            var expected = (Table.Row.Id, Table.Row.Name);
+
+            result.Print();
+            result.Should().Be(expected);
+        }
+    }
+}
